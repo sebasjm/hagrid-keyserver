6d3f2789148870f33ead3fd8b72c13e00fe91d83
Kai Michaelis <kai@sequoia-pgp.org>
2019-02-12 13:10:59 +0100

    missing build.rs file

---
+++ build.rs
@@ -0,0 +1,11 @@
+extern crate vergen;
+
+use vergen::{generate_cargo_keys, ConstantsFlags};
+
+fn main() {
+    let flags = ConstantsFlags::all();
+
+    // Generate the 'cargo:' key output
+    generate_cargo_keys(ConstantsFlags::all())
+        .expect("Unable to generate the cargo keys!");
+}
